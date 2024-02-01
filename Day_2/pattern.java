6fc6897c1e59fbf01015f1ded91840e3c4fda697
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-01 17:55:27 +0530

    Update pattern.java

--- Day_2/pattern.java
+++ Day_2/pattern.java
@@ -1,1 +1,25 @@
+public class Solution {
+    public static void numberCrown(int n) {
+        // Write your code here.
+        for (int i = 1; i <= n; i++) {
 
+            // Loop to print increasing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            // Loop to print spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+
+            // Loop to print decreasing numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            // Move to the next line after each row
+            System.out.println();
+        }
+    }
+}
