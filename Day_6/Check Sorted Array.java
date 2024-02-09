70629773bd534a8f7a6f11d5d0413bced0f3d3e9
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-09 18:28:50 +0530

    Create Check Sorted Array.java

---
+++ Day_6/Check Sorted Array.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static int isSorted(int n, int []arr) {
+        // Write your code here.
+        for(int i=0;i<n-1;i++){
+            if(arr[i]<=arr[i+1]){
+                continue;
+            }
+            else{
+                return 0;
+            }
+            
+        }
+        return 1;
+    }
+}
