1aba3250ce454d15cf9a64107b53029d422136bb
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-09 19:20:01 +0530

    Create Rotate Array.java

---
+++ Day_6/Rotate Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] arr, int k) {
+        
+        // Write your code here.
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer>ar = new ArrayList<>();
+        for(int i=0;i<k;i++){
+            a.add(arr.get(i));
+
+        }
+        for(int i=k;i<arr.size();i++){
+            ar.add(arr.get(i));
+            
+        }
+        for(int i=0;i<k;i++){
+            ar.add(a.get(i));
+            
+        }
+        return ar;
+    }
+}
+        
+    
