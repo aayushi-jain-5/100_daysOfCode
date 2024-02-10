7b4468e7c9cb8fed913445e10509bda82224be7e
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-10 17:52:44 +0530

    Create  Find Nth Root Of M.java

---
+++ Day_2/Find Nth Root Of M.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static int sqrtN(long N) {
+		/*
+		 * Write your code here
+		 */
+		 int x= (int)Math.sqrt(N);
+		 return x;
+	}
+}
