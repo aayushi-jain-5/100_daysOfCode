2cbe98833f6bb7c637241108bf03eb52df476b0f
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-10 17:33:40 +0530

    Create Rotation.java

---
+++ Day_8/Rotation.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public static int findKRotation(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+
+                // Eliminate left half:
+                low = mid + 1;
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+}
