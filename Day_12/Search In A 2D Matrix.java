29ac9e9486dc120467aeffbe2beebb8f65ad9b73
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-11 17:04:38 +0530

    Create Search In A 2D Matrix.java

---
+++ Day_12/Search In A 2D Matrix.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Solution {
+    static boolean searchMatrix(ArrayList<ArrayList<Integer>> matrix, int target) {
+        // Write your code here.
+         int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        //apply binary search:
+        int low = 0, high = n * m - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int row = mid / m, col = mid % m;
+            if (matrix.get(row).get(col) == target) return true;
+            else if (matrix.get(row).get(col) < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+}
