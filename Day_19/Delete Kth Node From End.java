36352a5f3d60b4e6c1c3b4c8a8ecc1d97336597f
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-18 18:01:22 +0530

    Create Delete Kth Node From End.java

---
+++ Day_19/Delete Kth Node From End.java
@@ -0,0 +1,63 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node removeKthNode(Node head, int N)
+    {
+        // Write your code here.
+        Node fastp = head;
+        Node slowp = head;
+
+        // Move the fastp pointer N nodes ahead
+        for (int i = 0; i < N; i++)
+            fastp = fastp.next;
+
+        // If fastp becomes null, the Nth node from the end is the head
+        if (fastp == null)
+            return head.next;
+
+        // Move both pointers until fastp reaches the end
+        while (fastp.next != null) {
+            fastp = fastp.next;
+            slowp = slowp.next;
+        }
+
+        // Delete the Nth node from the end
+        Node delNode = slowp.next;
+        slowp.next = slowp.next.next;
+        delNode = null;
+        return head;
+        
+    }
+}
