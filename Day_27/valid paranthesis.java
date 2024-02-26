933bd25e2de744cdd969886c087f8907f7517837
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-26 09:20:22 +0530

    Create valid paranthesis.java

---
+++ Day_27/valid paranthesis.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+public class Solution {
+
+    public static boolean isValidParenthesis(String s) {
+        // Write your code here.
+        Stack<Character> stack = new Stack<>();
+
+
+        // convert string into char array and access the characters using for each loop.
+        for(char ch: s.toCharArray())
+        {
+            // check ch
+            switch (ch)
+            {
+                // open bracket then push it in stack.
+                // close bracket then pop the item and compare.
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+
+                    // if the stack is empty then it means string have no open bracket.
+                        // hence it is invalid.
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
