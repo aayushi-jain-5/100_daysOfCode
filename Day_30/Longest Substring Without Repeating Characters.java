e6d6e0e4f69a43d361041c1f00d017151d8cb45e
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-02-29 23:52:12 +0530

    Create Longest Substring Without Repeating Characters.java

---
+++ Day_30/Longest Substring Without Repeating Characters.java
@@ -0,0 +1,29 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+	public static int uniqueSubstrings(String s) 
+    {
+		//write your code here
+		int n = s.length();
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+             else {
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
+            }
+        }
+        
+        return maxLength;
+	}
+}
