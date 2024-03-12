42fae16086263eabe7a37cf5979df65857b5a830
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-03-12 12:29:46 +0530

    Create Number Of Nodes.java

---
+++ Day_42/Number Of Nodes.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Solution {
+    public static int numberOfNodes(int N){
+        // Write your code here.
+        return (int)Math.pow((double)2,(double)N-1);
+    }
+}
